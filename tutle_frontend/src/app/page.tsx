be5af2c8748e72bd9db69bde6e05f47e0e6afcae
Yijungu <wnsrn4970@naver.com>
2023-06-24 16:04:49 +0900

    page create

--- tutle_frontend/src/app/page.tsx
+++ tutle_frontend/src/app/page.tsx
@@ -2,112 +2,100 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="e4_2">
+      <div className="e6_15">
+        <span className="e6_16">땅끝마을 바다거북수프 맛집</span>
+        <span className="e6_17">AI 기반 무한 바다거북수프</span>
+      </div>
+      <div className="e6_18">
+        <div className="e6_19"></div>
+        <span className="e6_20">시작하기 </span>
+      </div>
+      <div className="e19_10">
+        <div className="e6_22">
+          <div className="e6_23"></div>
+          <div className="e6_24"></div>
+        </div>
+        <div className="e6_28"></div>
+        <div className="e6_25"></div>
+        <div className="e6_26"></div>
+        <div className="e6_27"></div>
+        <div className="e12_16">
+          <div className="ei12_16_152_101"></div>
+          <span className="ei12_16_152_102">바닷가 레스토랑인게 중요합니까?</span>
+        </div>
+        <div className="e6_30">
+          <div className="ei6_30_152_95"></div>
+          <span className="ei6_30_152_96">네. 그 남자의 과거와 관련이 있습니다.</span>
+        </div>
+        <div className="e12_26">
+          <div className="ei12_26_152_95"></div>
+          <span className="ei12_26_152_96">네. 조금은 관계 있습니다.</span>
+        </div>
+        <div className="e6_29">
+          <div className="ei6_29_152_101"></div>
+          <span className="ei6_29_152_102">수프가 그 남자의 과거와 관련이 있나요? </span>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="e111_293">
+        <div className="e111_294">
+          <div className="ei111_294_144_2659"></div>
+        </div>
+        <span className="e111_295">F22F</span>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="e204_182">
+        <div className="e172_228"></div>
+        <div className="e172_234">
+          <div className="ei172_234_5790_3581"></div>
+        </div>
+        <span className="e172_236">TOP</span>
       </div>
-    </main>
+      <div className="e204_184">
+        <div className="e186_126"></div>
+        <div className="e186_133">
+          <div className="ei186_133_5790_5654"></div>
+        </div>
+        <div className="e186_114">
+          <div className="ei186_114_125_158"></div>
+        </div>
+        <span className="e186_197">닉네임을 입력하세요.</span>
+      </div>
+      <div className="e186_174">
+        <span className="e186_175">Q. 바다거북수프란 무엇인가요?</span>
+      </div>
+      <div className="e186_176">
+        <span className="e186_177">질문과 답변</span>
+      </div>
+      <span className="e186_178">A. 전세계 곳곳에서 유행 중인 나폴리탄 괴담 계열의 수수께끼입니다. 
+      보통 기묘한 내용을 바탕으로 만들어지는 추리 놀이입니다.</span>
+      <div className="e186_179">
+        <span className="e186_180">Q. 바다거북수프를 하는 방법은 무엇인가요?</span>
+      </div>
+      <span className="e186_181">A. 출제자가 이야기 형식의 수수께끼를 만들어 출제합니다. 참가자들은 출제자에게 예 또는 아니오로 대답할 수 있는 질문을 하고, 출제자는 그에 대한 대답을 합니다.                                                                                                           
+정보를 통해 참가자들은 이야기의 전말을 추리합니다. 가장 답에 가까운 질문을 하게 되는 때가 오면 출제자는 정답을 공개합니다.</span>
+      <div className="e186_182">
+        <span className="e186_183">Q. AI가 어떻게 정답임을 인지하나요?</span>
+      </div>
+      <span className="e186_184">A. ??</span>
+      <div className="e186_185">
+        <span className="e186_186">Q. 여태 플레이한 기록을 볼 수 있나요?</span>
+      </div>
+      <span className="e186_187">A. 플레이한 기록은 우측 상단의 ‘log’에서 볼 수 있습니다. 홈 화면에서 닉네임을 입력하면 어쩌고~</span>
+      <div className="e186_188">
+        <span className="e186_189">Q. 바다거북수프 이야기는 어떻게 정해지나요?</span>
+      </div>
+      <span className="e186_190">A. ??</span>
+      <div className="e186_191">
+        <span className="e186_192">Q. 하루에 한 번 이상 플레이할 수 없나요?</span>
+      </div>
+      <span className="e186_193">A. 바다거북수프는 하루에 한 번만 플레이 할 수 있습니다. 저희는 꼬맨틀과 같은 게임의 핵심 요소는 &quot;하루에 한 번만, 모두가 동일한 정답&quot;이라고 생각합니다. 
+     정답 단어는 국제 표준시(UTC) 기준 매일 오후 3시 또는 한국 표준시(KST) 기준 자정에 바뀝니다.
+</span>
+      <div className="e186_194">
+        <span className="e186_195">Q. 다른 질문이나 피드백은 어떻게 보내나요?</span>
+      </div>
+      <span className="e186_196">A. 질문이나 피드백은 F22F 메일로 문의해주세요!</span>
+    </div>
   )
 }
+
