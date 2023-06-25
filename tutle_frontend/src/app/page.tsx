ac9ffa5b6a019c4ce8b566476de5bfaafab17f0a
Yijungu <wnsrn4970@naver.com>
2023-06-25 14:11:36 +0900

    page2, 3 css html create

--- tutle_frontend/src/app/page.tsx
+++ tutle_frontend/src/app/page.tsx
@@ -61,40 +61,40 @@ export default function Home() {
         <span className="e186_197">닉네임을 입력하세요.</span>
       </div>
       <div className="e186_174">
-        <span className="e186_175">Q. 바다거북수프란 무엇인가요?</span>
+        <p className="e186_175">Q. 바다거북수프란 무엇인가요?</p>
+        <p className="e186_178">A. 전세계 곳곳에서 유행 중인 나폴리탄 괴담 계열의 수수께끼입니다.</p>
+        <p className="e186_178_2">보통 기묘한 내용을 바탕으로 만들어지는 추리 놀이입니다.</p>
       </div>
       <div className="e186_176">
         <span className="e186_177">질문과 답변</span>
       </div>
-      <span className="e186_178">A. 전세계 곳곳에서 유행 중인 나폴리탄 괴담 계열의 수수께끼입니다. 
-      보통 기묘한 내용을 바탕으로 만들어지는 추리 놀이입니다.</span>
       <div className="e186_179">
-        <span className="e186_180">Q. 바다거북수프를 하는 방법은 무엇인가요?</span>
+        <p className="e186_175">Q. 바다거북수프를 하는 방법은 무엇인가요?</p>
+        <p className="e186_178">A. 출제자가 이야기 형식의 수수께끼를 만들어 출제합니다. 참가자들은 출제자에게 예 또는 아니오로 대답할 수 있는 질문을 하고, 출제자는 그에 대한 대답을 합니다.                                                                                                           
+정보를 통해 참가자들은 이야기의 전말을 추리합니다. 가장 답에 가까운 질문을 하게 되는 때가 오면 출제자는 정답을 공개합니다.</p>
       </div>
-      <span className="e186_181">A. 출제자가 이야기 형식의 수수께끼를 만들어 출제합니다. 참가자들은 출제자에게 예 또는 아니오로 대답할 수 있는 질문을 하고, 출제자는 그에 대한 대답을 합니다.                                                                                                           
-정보를 통해 참가자들은 이야기의 전말을 추리합니다. 가장 답에 가까운 질문을 하게 되는 때가 오면 출제자는 정답을 공개합니다.</span>
       <div className="e186_182">
-        <span className="e186_183">Q. AI가 어떻게 정답임을 인지하나요?</span>
+        <p className="e186_175">Q. AI가 어떻게 정답임을 인지하나요?</p>
+        <p className="e186_178">A. ??</p>
       </div>
-      <span className="e186_184">A. ??</span>
       <div className="e186_185">
-        <span className="e186_186">Q. 여태 플레이한 기록을 볼 수 있나요?</span>
+        <span className="e186_175">Q. 여태 플레이한 기록을 볼 수 있나요?</span>
+        <span className="e186_178">A. 플레이한 기록은 우측 상단의 ‘log’에서 볼 수 있습니다. 홈 화면에서 닉네임을 입력하면 어쩌고~</span>
       </div>
-      <span className="e186_187">A. 플레이한 기록은 우측 상단의 ‘log’에서 볼 수 있습니다. 홈 화면에서 닉네임을 입력하면 어쩌고~</span>
       <div className="e186_188">
-        <span className="e186_189">Q. 바다거북수프 이야기는 어떻게 정해지나요?</span>
+        <span className="e186_175">Q. 바다거북수프 이야기는 어떻게 정해지나요?</span>
+        <span className="e186_178">A. ??</span>
       </div>
-      <span className="e186_190">A. ??</span>
       <div className="e186_191">
-        <span className="e186_192">Q. 하루에 한 번 이상 플레이할 수 없나요?</span>
-      </div>
-      <span className="e186_193">A. 바다거북수프는 하루에 한 번만 플레이 할 수 있습니다. 저희는 꼬맨틀과 같은 게임의 핵심 요소는 &quot;하루에 한 번만, 모두가 동일한 정답&quot;이라고 생각합니다. 
+        <span className="e186_175">Q. 하루에 한 번 이상 플레이할 수 없나요?</span>
+        <span className="e186_178">A. 바다거북수프는 하루에 한 번만 플레이 할 수 있습니다. 저희는 꼬맨틀과 같은 게임의 핵심 요소는 &quot;하루에 한 번만, 모두가 동일한 정답&quot;이라고 생각합니다. 
      정답 단어는 국제 표준시(UTC) 기준 매일 오후 3시 또는 한국 표준시(KST) 기준 자정에 바뀝니다.
 </span>
-      <div className="e186_194">
-        <span className="e186_195">Q. 다른 질문이나 피드백은 어떻게 보내나요?</span>
       </div>
-      <span className="e186_196">A. 질문이나 피드백은 F22F 메일로 문의해주세요!</span>
+      <div className="e186_194">
+        <span className="e186_175">Q. 다른 질문이나 피드백은 어떻게 보내나요?</span>
+        <span className="e186_178">A. 질문이나 피드백은 F22F 메일로 문의해주세요!</span>
+      </div>
     </div>
   )
 }
---
+++ tutle_frontend/src/app/problem/page.tsx
@@ -0,0 +1,42 @@
+import './page.css';
+
+export default function Problem() {
+    return (
+    <div className="e9_52">
+  <div className="e218_192">
+    <div className="e125_118">
+      <div className="ei125_118_144_2659"></div>
+      <div className="ei125_118_152_168">
+        <div className="ei125_118_152_135"></div>
+        <div className="ei125_118_152_122"></div>
+      </div>
+    </div><span  className="e28_53">정답. 남편은 병원에서 의사에 의해 죽게 되었습니다.</span>
+  </div>
+  <div className="e28_163">
+    <div className="e111_301"><span  className="e28_68">thisis2jun9 님</span>
+      <div className="e125_157">
+        <div className="e125_158"></div>
+      </div>
+    </div><span  className="e125_142">About                    QnA                  Log</span>
+    <div className="e125_159">
+      <div className="ei125_159_144_2659"></div>
+    </div><span  className="e102_34">F22F</span>
+  </div>
+  <div className="e168_70"><span  className="e168_73">텍스트 입력 칸에 추측한 내용을 적으면 ‘네’ 또는 ‘아니오’ 형식의 답을 받을 수 있습니다.
+N번째 바다거북수프의 정답을 맞혀보세요.</span></div>
+  <div className="e218_180">
+    <div className="e102_77">
+      <div className="ei102_77_152_95"></div>
+    </div><span  className="e19_34">여자는 귀가하기 위해 엘리베이터를 타려다가,
+자신의 남편이 죽은 것을 깨달았다. 왜 그랬을까?</span>
+  </div>
+  <div className="e218_179">
+    <div className="e186_106"></div>
+    <div className="e186_107">
+      <div className="ei186_107_5790_3581"></div>
+    </div><span  className="e186_108">TOP</span>
+  </div>
+</div>
+    )
+}
+
