5f3976cbb65513e8f3cbf7a776caca2548f55f71
앵 <ngong@aeng-ui-MacBookAir.local>
2023-06-27 11:04:48 +0900

    :zap: html css 꾸미기

--- turtle_frontend/src/page/thanks/page.js
+++ turtle_frontend/src/page/thanks/page.js
@@ -7,17 +7,18 @@ export default function Thanks() {
     <div className="e172_65">
       <div className="ei172_65_152_95"></div>
     </div>
-    <span  className="e108_103">남편은 병으로 산소호흡기와 같은 의료 기구를 달고 있었는데 병원 전체가 정전이 되어 죽게 되었고, 
+    <span  className="user_answer">남편은 병으로 산소호흡기와 같은 의료 기구를 달고 있었는데 병원 전체가 정전이 되어 죽게 되었고, 
 아내는 작동되지 않는 엘리베이터를 보고 그 사실을 깨닫게 되었다.</span>
   </div>
   <div className="e154_160">
     <div className="e154_144"></div>
-    <div className="e154_146"></div><span  className="e154_152">N번째 추측만에 정답을 맞혔습니다!
-[나의 플레이]
-가장 처음 풀었던 바다거북수프 번호: n     도전한 게임 횟수: n
-정답 횟수: 6    포기 횟수: 0
-지금까지 물어본 총 질문 개수: n
-N+1번째 바다거북은 오늘 밤 자정(한국 시간 기준)에 찾아옵니다.</span>
+    <div className="e154_146"></div>
+    <span  className="my_play">N번째 추측만에 정답을 맞혔습니다!</span>
+    <span  className="my_play_2">[나의 플레이]</span>
+    <span  className="my_play_3">가장 처음 풀었던 바다거북수프 번호: n     도전한 게임 횟수: n</span>
+    <span  className="my_play_4">정답 횟수: 6    포기 횟수: 0</span>
+    <span  className="my_play_5">지금까지 물어본 총 질문 개수: n</span>
+    <span  className="my_play_6">N+1번째 바다거북은 오늘 밤 자정(한국 시간 기준)에 찾아옵니다.</span>
   </div>
   <div className="e218_229">
     <div className="e168_85">
@@ -28,7 +29,7 @@ N+1번째 바다거북은 오늘 밤 자정(한국 시간 기준)에 찾아옵�
     <div className="e172_179">
       <div className="ei172_179_327_14292"></div>
       <div className="ei172_179_327_14293"></div>
-    </div><span  className="e106_101">심장병이 있던 남편은 전기 의료 기구에 의존해서 살고 있었습니다. 여느 때처럼 남편의 병문안을 갔다온 
+    </div><span  className="AI_answer">심장병이 있던 남편은 전기 의료 기구에 의존해서 살고 있었습니다. 여느 때처럼 남편의 병문안을 갔다온 
 아내는 건물 전체의 정전으로 엘리베이터가 멈춘 것을 보고 남편이 죽은 것을 깨닫게 되었습니다.</span>
   </div>
   <div className="e218_202">
@@ -37,14 +38,18 @@ N+1번째 바다거북은 오늘 밤 자정(한국 시간 기준)에 찾아옵�
     </div>
   </div>
   <div className="e218_216">
-    <div className="e218_217"><span  className="e218_218">thisis2jun9 님</span>
+    <div className="e218_217">
+    <span  className="nickname">thisis2jun9 님</span>
       <div className="e218_219">
         <div className="ei218_219_125_158"></div>
       </div>
-    </div><span  className="e218_220">About                    QnA                  Log</span>
+    </div>
+    <p  className="About">About</p>
+    <p  className="QnA">QnA</p>
+    <p  className="Log">Log</p>
     <div className="e218_221">
       <div className="ei218_221_144_2659"></div>
-    </div><span  className="e218_222">F22F</span>
+    </div><span  className="F22F">F22F</span>
   </div>
 </div>
   )
