41592fa4953e57036fde0b91b2659ea9b079f8bb
Yijungu <wnsrn4970>
2023-07-03 16:25:09 +0900

    프론트엔드 기능 추가 전

--- turtle_frontend/src/component/qna.js
+++ turtle_frontend/src/component/qna.js
@@ -1,7 +1,7 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import AIIcon from "../images/AIIcon.png"; // 이미지 import
-import SeaTurtleIcon from "../images/SeaTurtleIcon.png"; // 이미지 import
+import AiIcon from "../images/AiIcon.png"; // 이미지 import
+import UserIcon from "../images/UserIcon.png"; // 이미지 import
 
 const QnA = ({ question, answer, borderStrength, borderBottomStrength }) => {
   return (
@@ -28,11 +28,11 @@ const QnA = ({ question, answer, borderStrength, borderBottomStrength }) => {
         }}
       >
         <img
-          src={SeaTurtleIcon}
+          src={UserIcon}
           alt="SeaTurtle"
-          width="25"
-          height="25"
-          style={{ marginLeft: "10px", marginRight: "10px" }}
+          width="26"
+          height="27"
+          style={{ marginLeft: "10px", marginRight: "10px", transform: "rotate(11deg)" }}
         />
         <Typography>{question}</Typography>
       </Box>
@@ -52,7 +52,7 @@ const QnA = ({ question, answer, borderStrength, borderBottomStrength }) => {
         }}
       >
         <img
-          src={AIIcon}
+          src={AiIcon}
           alt="AI"
           width="25"
           height="25"
